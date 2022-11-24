e2c27e9cc8151e5c39310e360851002b86eeadc3
Susi Tan <69844885+susitan@users.noreply.github.com>
2022-11-24 17:12:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-Omega 3
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
